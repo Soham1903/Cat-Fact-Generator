5d70d02c5d5ba0d86641799ddd6e0f64e007a1d9
Soham Anand Ladgaonkar <95856008+Soham1903@users.noreply.github.com>
2024-09-24 21:39:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
 
-import Todolist from "./components/Todolist";
 import Catfact from "./components/Catfact";
-import "./CSS/Todolist.css";
 import "./CSS/Catfact.css";
 
 function App() {
   return (
     <div className="App">
-      {/* <Todolist /> */}
       <Catfact />
     </div>
   );
